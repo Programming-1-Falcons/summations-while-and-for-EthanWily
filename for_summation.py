ef581d86e11752dfa942e688a6293e824739d1f8
EthanWily <wilyrob001@alpinesd.org>
2024-10-22 11:26:02 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -2,9 +2,7 @@ number = int(input("Please enter a number: "))
 i = 0
 sum = 0
 for i in range(number):
-      print("i 1:", i)
       i = i + 1
       sum = sum + i
-      print("i 2:", i)
 
 print("i =", sum)
