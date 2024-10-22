730e9fad06d347d092e4c46ebc1e7317cc018218
EthanWily <wilyrob001@alpinesd.org>
2024-10-22 11:18:19 -0600

    while summation yay
    I did the While Summation :D

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
-#while summation code following directions from Readme
+number = int(input("Please enter a number: "))
+i = 1
+num = 1
+sum = 0
+while i <= number:
+    sum = sum + num
+    num = num + 1
+    i = i + 1
+print(sum)
